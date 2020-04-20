dacdae28bce4991c5cbea378574d30f36818285e
William Mbotta <sergembotta@mailoo.org>
2020-04-21 00:53:08 +0200

    init

---
+++ index.js
@@ -0,0 +1,54 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const url = "https://youtube.com/feed/history";
+
+(async () => {
+  const cookies = JSON.parse(fs.readFileSync("cookie.json", "utf8"));
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+
+  const page = await browser.newPage();
+
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
+  );
+
+  await page.setCookie(...cookies);
+  await page.goto(url);
+
+  const delay = 2600;
+  let preCount = 0;
+  let postCount = 0;
+  let count = 0;
+
+  do {
+    count++;
+    preCount = await getCount(page);
+    await scrollDown(page);
+    await page.waitFor(delay);
+    postCount = await getCount(page);
+  } while (postCount > preCount);
+  await page.waitFor(delay);
+
+  const data = await getLinks(page);
+  fs.writeFileSync("data.json", JSON.stringify(data));
+
+  await browser.close();
+})();
+
+async function getCount(page) {
+  return await page.$$eval("#thumbnail", (a) => a.length);
+}
+
+async function getLinks(page) {
+  return await page.$$eval("#video-title", (nodes) =>
+    nodes.map(({ href, title }) => ({ href, title }))
+  );
+}
+
+async function scrollDown(page) {
+  await page.$eval("ytd-item-section-renderer:last-child", (e) => {
+    e.scrollIntoView({ behavior: "smooth", block: "end", inline: "end" });
+  });
+}
